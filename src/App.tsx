6d5d9504d365142ef151587d81fd9324f566299a
Steve Bastians <stevejb71@gmail.com>
2019-03-10 21:35:06 +0800

    clicking adds a black piece to the square clicked

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,24 @@ import * as React from 'react';
 import './App.css';
 import { BoardContainer } from './Board-container'
 import { Player } from './MoveCalc';
+import { BoardContent, SquareContent } from './Model';
+import { UpdateBoardAction } from './redux/Actions';
+import { store } from './redux/Store';
 
-export default function App() {
+interface AppProps {
+  board: BoardContent,
+  updateBoard: (board: BoardContent) => UpdateBoardAction
+}
+
+export default function App(props: AppProps) {
+  const onClick = doUpdate(props.board, props.updateBoard)
   return (
-      <BoardContainer size={8} player={Player.Black} onClick={() => {}}/>
+      <BoardContainer size={8} player={Player.Black} onClick={onClick}/>
   )
+}
+
+const doUpdate = (board: BoardContent, updateBoard: (board: BoardContent) => UpdateBoardAction) => (index: number) => {
+  const boardCopy = board.slice(0)
+  boardCopy[index] = SquareContent.Black
+  store.dispatch(updateBoard(boardCopy))
 }
